d7b3e7c3bab504728febffad44fd961ae64f65b1
nwoik <nwokoyed@gmail.com>
2025-12-22 12:33:03 +0000

    table handler completed

--- handlers/enablecors.go
+++ handlers/enablecors.go
@@ -2,7 +2,7 @@ package handlers
 
 import "net/http"
 
-func EnableCors(w *http.ResponseWriter) {
+func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Push-Key, Push-Token")
